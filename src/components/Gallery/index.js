8df7be6eb1a9158111fef02ba84ec444a89972db
Ben H <deepfreespace@gmail.com>
2022-01-27 18:25:57 -0700

    More module BS fixes

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,15 +1,3 @@
-import React from 'react';
-import PhotoList from '../PhotoList';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import Gallery from './Gallery';
 
-function Gallery(props) {
-  const { currentCategory } = props;
-  return (
-    <section>
-      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.description}</p>
-      <PhotoList category={currentCategory.name} />
-    </section>
-  );
-}
 export default Gallery;
